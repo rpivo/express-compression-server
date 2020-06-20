19a099380ed58a4cf329a66c00dfc758db8158d0
Ryan Pivovar <ryanpivovar@gmail.com>
2020-06-20 15:35:17 -0400

    set up yargs

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 "use strict";
-console.log('hello');
+var express = require('express');
+var expressStaticGzip = require('express-static-gzip');
+var yargs = require('yargs');
+var options = yargs
+    .usage('Usage: -b <build>')
+    .option('b', {
+    alias: 'build',
+    describe: 'location of build to be served',
+    type: 'string',
+    demandOption: true,
+})
+    .argv;
+var build = options.build;
+console.log(build);
+// const app = express();
+// const port = 3000; // add dynamic import to yargs
+// app.use('/', expressStaticGzip('/', {
+//   enableBrotli: true,
+// }));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
-console.log('hello');
+const express = require('express');
+const expressStaticGzip = require('express-static-gzip');
+const yargs = require('yargs');
+
+const options = yargs
+  .usage('Usage: -b <build>')
+  .option('b', {
+    alias: 'build',
+    describe: 'location of build to be served',
+    type: 'string',
+    demandOption: true,
+  })
+  .argv;
+
+  const { build } = options;
+
+  console.log(build);
+
+// const app = express();
+// const port = 3000; // add dynamic import to yargs
+
+// app.use('/', expressStaticGzip('/', {
+//   enableBrotli: true,
+// }));
