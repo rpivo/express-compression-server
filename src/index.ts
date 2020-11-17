146b2dd3addd7b79615a15a1758b06c13ad8ccd2
Ryan <ryanpivovar@gmail.com>
2020-11-17 00:58:25 -0500

    remove yargs as a dependency

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,11 @@ import browserSync from 'browser-sync';
 import express from 'express';
 import expressStaticGzip from 'express-static-gzip';
 import path from 'path';
-import yargs from 'yargs';
 
-type Options = {
-  build: string;
-  port?: number;
-};
+let [processBuild, processPort] = process.argv.slice(2);
 
-const options = yargs
-  .usage('Usage: -b <build> -p <port>')
-  .option('b', {
-    alias: 'build',
-    describe: 'location of build to be served',
-    type: 'string',
-    demandOption: true,
-  })
-  .option('p', {
-    alias: 'port',
-    describe: 'port to be used for server',
-    type: 'number',
-  })
-  .argv as unknown as Options; // todo: clean up this type
-
-const { build, port = 1235 } = options;
+const build = processBuild?.split('=')[1] ?? 'dist';
+const port = processPort?.split('=')[1] ?? 1235;
 
 const cwd = path.resolve();
 const buildPath = `${cwd}/${build}`;
@@ -42,7 +24,7 @@ browserSync.init({
   logLevel: 'silent',
   notify: false,
   open: true,
-  port,
+  port: +port,
   proxy: serverPath,
   reloadOnRestart: true,
   watch: true,
