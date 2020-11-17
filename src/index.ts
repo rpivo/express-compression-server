4db0fd8f46629f209b881ed658e7421ec35ce8be
Ryan <ryanpivovar@gmail.com>
2020-11-17 15:20:51 -0500

    fix issue where params would only work when in a specific order

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,51 @@ import express from 'express';
 import expressStaticGzip from 'express-static-gzip';
 import path from 'path';
 
-let [processBuild, processPort] = process.argv.slice(2);
+enum DefaultParams {
+  '--build' = 'dist',
+  '--port' = '1000',
+}
 
-const build = processBuild?.split('=')[1] ?? 'dist';
-const port = processPort?.split('=')[1] ?? 1235;
+enum Params {
+  BUILD = '--build',
+  PORT = '--port',
+}
+
+const paramMap: {
+  '--build'?: string;
+  '--port'?: string;
+} = {};
+
+const [...params] = process.argv.slice(2);
+
+params.forEach(param => {
+  const [key, value] = param.split('=');
+  paramMap[key] = value;
+});
+
+for (const key in DefaultParams) paramMap[key] ??= DefaultParams[key];
+
+const {
+  [Params.BUILD]: build,
+  [Params.PORT]: port,
+} = paramMap;
 
 const cwd = path.resolve();
 const buildPath = `${cwd}/${build}`;
-const serverPath = `http://localhost:${port + 1}`;
+const serverPath = `http://localhost:${port! + 1}`;
 
 const app = express();
 app.use('/', expressStaticGzip(`${build}`, {
   enableBrotli: true,
 }));
-app.listen(port + 1, () => console.log(`\nbuild: http://localhost:${port}/index.html`));
+app.listen(port! + 1, () => console.log(`\nbuild: http://localhost:${port}/index.html`));
 
 browserSync.init({
   browser: 'google chrome',
   logLevel: 'silent',
   notify: false,
   open: true,
-  port: +port,
+  port: +port!,
   proxy: serverPath,
   reloadOnRestart: true,
   watch: true,
